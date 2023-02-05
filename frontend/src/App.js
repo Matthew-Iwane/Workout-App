9c174c22caa6f5ea10f770e0be088fdd8610d35d
Matthew T. Iwane <mattiwane@gmail.com>
2023-02-04 19:23:06 -0500

    restarted create-react-app, did not fully load in before

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+import {Home} from "./components/Home.jsx"
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
